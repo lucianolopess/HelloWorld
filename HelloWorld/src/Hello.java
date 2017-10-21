ecf452051726d52c522dac171b10464c79164bb3
Luciano <Luciano@Asus-Casa>
2017-10-21 16:13:00 -0200

    first implementation of Hello class

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Luciano
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
